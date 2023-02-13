cbab0aa7874825785fc492b2fe40b6659aacd9ff
Kamil Studzinski <kamil.studzinski@flyeralarm-industrialprint.com>
2023-02-13 15:31:10 +0100

    Including user repositories

--- github-finder-client/src/App.js
+++ github-finder-client/src/App.js
@@ -7,6 +7,7 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
+import User from "./pages/User";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
                 <Route path="/about" element=<About /> />
                 <Route path="/notfound" element=<NotFound /> />
                 <Route path="/*" element=<NotFound /> />
+                <Route path="/user/:login" element=<User /> />
               </Routes>
             </main>
             <Footer />
--- github-finder-client/src/context/github/GithubContext.js
+++ github-finder-client/src/context/github/GithubContext.js
@@ -8,6 +8,8 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   };
 
@@ -15,7 +17,24 @@ export const GithubProvider = ({ children }) => {
 
   const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
 
-  // Get initial users (testing purposes)
+  // Fetch single user
+  const fetchUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`);
+
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const profile = await response.json();
+      dispatch({
+        type: "FETCH_USER",
+        payload: profile,
+      });
+    }
+  };
+
+  // Get the user list
   const searchUsers = async (text) => {
     setLoading();
     const params = new URLSearchParams({
@@ -31,6 +50,27 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  // Get user repos
+
+  const GetUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`
+    );
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    });
+  };
+
   const setLoading = () => dispatch({ type: "SET_LOADING" });
 
   return (
@@ -38,8 +78,12 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
         searchUsers,
+        repos: state.repos,
         clearUsers,
+        fetchUser,
+        GetUserRepos,
       }}
     >
       {children}
